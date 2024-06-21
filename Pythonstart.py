4242900abfc053ed937f8ff9ae1cc1cc5f84f9c0
pthakur11205 <pthakur@uw.edu>
2024-06-21 17:20:52 -0400

    Creating function for endless web scraping

--- Pythonstart.py
+++ Pythonstart.py
@@ -2,16 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-resp = requests.get(
-	url="https://en.wikipedia.org/wiki/Batman",
-)
+#Recursive function that scrapes an article from the given URL/current article
+#url - access to the wiki article
+def scrapeArticle(url): 
+    resp = requests.get(
+        url=url,
+    )
 
-soup = BeautifulSoup(resp.content, 'html.parser')
+    soup = BeautifulSoup(resp.content, 'html.parser')
 
-# Extracting the title from the HTML
-title = soup.find(id="firstHeading")
-print(title.string)
+    # Extracting the title from the HTML
+    title = soup.find(id="firstHeading")
 
-# Get all the links from <a> tags
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
+    # Get all the links from <a> tags
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScrape = 0
+
+    for link in allLinks:
+        #To find links to other wiki articles
+        if link['href'].find("/wiki/") == -1:
+            continue
+
+        # Gets random wiki link to a different article from within the original article
+        linkToScrape = link
+        break
+
+    #Calls the method on the selected article
+    scrapeArticle("https://en.wikipedia.org" + linkToScrape['href'])
+

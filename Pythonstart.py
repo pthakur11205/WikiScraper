23a8401e66a0a538204f3963d733af23d180f188
pthakur11205 <pthakur@uw.edu>
2024-06-21 17:12:48 -0400

    Extracting title and links

--- Pythonstart.py
+++ Pythonstart.py
@@ -1,1 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
+import random
+
+resp = requests.get(
+	url="https://en.wikipedia.org/wiki/Batman",
+)
+
+soup = BeautifulSoup(resp.content, 'html.parser')
+
+# Extracting the title from the HTML
+title = soup.find(id="firstHeading")
+print(title.string)
+
+# Get all the links from <a> tags
+allLinks = soup.find(id="bodyContent").find_all("a")
+random.shuffle(allLinks)
